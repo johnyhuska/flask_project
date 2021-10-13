e8775eb1237f9ed502800fffaf9edbaf5372cfac
johnyhuska <johnyhuska@gmail.com>
2021-10-13 15:37:28 +0000

    loggin form + session management

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
+from flask import session
+from flask import url_for
 
 from .database import articles
 
 flask_app = Flask(__name__)
+flask_app.secret_key = b'\x92q\x1f\x9f&*\xac\xd3\xc6\x08z\xdc\xa9\xfeG\x89<\xdc\x010D\xfe\x8b\x84'
 
 @flask_app.route("/")
 def view_welcome_page():
@@ -27,3 +32,22 @@ def view_article(art_id):
     if article:
         return render_template("article.jinja", article=article)
     return render_template("article_not_found.jinja", art_id=art_id)
+
+@flask_app.route("/login/", methods=["GET"])
+def view_login():
+    return render_template("login.jinja")
+
+@flask_app.route("/login/", methods=["POST"])
+def login_user():
+    username = request.form["username"]
+    password = request.form["password"]
+    if username == "admin" and password =="admin":
+        session["logged"] = True
+        return redirect(url_for("view_admin"))
+    else:
+        return redirect(url_for("view_login"))
+
+@flask_app.route("/logout/", methods=["POST"])
+def logout_user():
+    session.pop("logged")
+    return redirect(url_for("view_welcome_page"))
